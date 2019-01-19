045e736e5a1f0b8d55cea2b89484959307e8d829
Peter Rice <peter@peterrice.xyz>
2019-01-19 16:27:36 -0500

    git root highlights don't work well with symlinks

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -6,8 +6,6 @@ function fish_prompt
     set color_normal (set_color normal)
     set color_reset (set_color normal)
     set color_git_root (set_color cyan)
-    set color_git_dirty 'todo'
-    set color_git_staged 'todo'
 
     # if test $status_copy != 0
     #     set color_white $color_error
@@ -18,12 +16,14 @@ function fish_prompt
     # end
 
     if git_is_repo
-        set git_root "$color_reset$color_git_root"(basename (git_repository_root))"$color_normal"
-        set git_root_idx (count (string split / (string replace ~ \~ (git_repository_root))))
+        # set git_root "$color_reset$color_git_root"(basename (git_repository_root))"$color_normal"
+        # TODO problem: git_repository_root is the real path, $PWD uses a virtual path that reflects symlinks
+        # TODO may be able to find root virtual PWD root by walking up the tree and checking if realpath == git_root
+        # set git_root_idx (count (string split / (string replace ~ \~ (git_repository_root))))
         if git_is_dirty
-            set glyph " $color_error\$$color_reset"
+            set glyph " $color_error\$"
         else if git_is_staged
-            set glyph ' '(set_color green)"\$$color_reset"
+            set glyph ' '(set_color green)"\$"
         else
             set glyph " $color_normal\$"
         end
@@ -37,16 +37,16 @@ function fish_prompt
 
     set pwd (string replace ~ \~ $PWD)
     if test $PWD = ~
-        if set -q git_root
-            set pwd $PWD
-        else
+        # if set -q git_root
+        #     set pwd $PWD
+        # else
             set prompt "$color_command~$color_normal"
-    end
+        # end
     else if test $PWD = /
         set prompt "$color_command/$color_normal"
-        if set -q git_root
-            set prompt $color_git_root$prompt
-        end
+        # if set -q git_root
+        #     set prompt $color_git_root$prompt
+        # end
     end
 
     if not set -q prompt
@@ -57,19 +57,19 @@ function fish_prompt
 
         set color_init $color_command
 
-        if set -q git_root_idx
-            if test $git_root_idx = 0
-                set color_init $color_git_root$color_init
-            else
-                set paths[$git_root_idx] $git_root
-            end
+        # if set -q git_root_idx
+        #     if test $git_root_idx = 0
+        #         set color_init $color_git_root$color_init
+        #     else
+        #         set paths[$git_root_idx] $git_root
+        #     end
 
-            if test $git_root_idx != (count $paths)
-                set paths[-1] $color_white(basename (pwd))$color_normal
-            end
-        else
+        #     if test $git_root_idx != (count $paths)
+        #         set paths[-1] $color_white(basename (pwd))$color_normal
+        #     end
+        # else
             set paths[-1] $color_white(basename (pwd))$color_normal
-        end
+        # end
 
         set prompt (string join / $paths | sed "s|^.|$color_init&$color_normal|")
     end
