12a8bf458ad510c5bc27439d48e8bb00ae174682
Peter Rice <peter@peterrice.xyz>
2019-01-20 01:24:30 -0500

    always show errors in right prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,25 +1,11 @@
 function fish_right_prompt
-    set -l status_copy $status
-    set -l status_code $status_copy
+    set -l status_code $status
 
     set -l color_normal (set_color normal)
     set -l color_error (set_color $fish_color_error)
     set -l color "$color_normal"
 
-    switch "$status_copy"
-        case 0 "$__mono_status_last"
-            set status_code
+    if test "$status_code" != 0
+        echo -n "$color_error($status_code)$color_normal "
     end
-
-    set -g __mono_status_last $status_copy
-
-    if test "$status_copy" -ne 0
-        set color "$color_error"
-    end
-
-    if test ! -z "$status_code"
-        echo -sn "$color($status_code)$color_normal "
-    end
-
-    echo -sn "$color$VIRTUAL_ENV$color_normal "
 end
