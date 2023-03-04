a685c0233b0384c94fe72dff867ce85d96f2f644
Peter Rice <peter@peterrice.xyz>
2023-03-04 10:39:05 -0500

    don't depend on sed

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -94,7 +94,7 @@ function fish_prompt
         set host '['(hostname -s)'] '
     end
 
-    set git (fish_git_prompt | sed "s/(/($color_yellow/;s/)/$color_normal)/")
+    set git (fish_git_prompt | string replace -r "\((.*)\)" "($color_yellow\$1$color_normal)")
 
     printf " $color_normal$host$root$color_normal$prompt$color_normal$git$glyph "
 end
