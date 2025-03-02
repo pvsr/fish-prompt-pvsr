2ac4b8d64c68da6b6fed16ca957f8176f494b931
Peter Rice <peterrice@fastmail.com>
2025-03-02 04:58:11 -0500

    check each parent for git root if symlinked
    if relative_vcs_root doesn't match there's a
    symlink involved and afaict the only way to find
    the root in the logical path is to check each
    component real path for an exact match.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -10,20 +10,36 @@ function fish_prompt
         set prompt_char '!'
     end
 
-    if type -q git && set vcs_root (git rev-parse --show-toplevel 2> /dev/null)
+    if type -q git && set real_vcs_root (git rev-parse --show-toplevel 2> /dev/null)
         set vcs git
     else if which hg >/dev/null 2>&1 && test -d .hg # FIXME only works at top level
         set vcs hg
-        set vcs_root (hg root)
+        set real_vcs_root (hg root)
     end
 
     if set -q vcs
         set color_path (set_color (string replace -r '(-o|--bold)' '' -- "$fish_color_command" | string trim))
         set color_path_basename (set_color $fish_color_command -o)
         set color_path_highlight (set_color $fish_color_param -o)
-        set relative_vcs_root (path normalize "$PWD/"(realpath --relative-to=(pwd -P) $vcs_root))
-        if test (realpath $relative_vcs_root) = $vcs_root
+        set relative_vcs_root (path normalize "$PWD/"(realpath --relative-to=(pwd -P) $real_vcs_root))
+        if test (realpath $relative_vcs_root) = $real_vcs_root
             set vcs_root $relative_vcs_root
+        end
+        while not set -q vcs_root
+            set -q logical_parent
+            or set logical_parent (dirname $PWD)
+            set real_sym_parent (realpath $logical_parent)
+            if test $real_sym_parent = $real_vcs_root
+                set vcs_root $real_sym_parent
+            else if not string match -q "$real_vcs_root*" $real_sym_parent
+                break
+            else if test logical_parent = /
+                break
+            else
+                set logical_parent (dirname $logical_parent)
+            end
+        end
+        if set -q vcs_root
             set vcs_basename $color_path_highlight(basename $vcs_root)$color_normal$color_path
             if test $vcs_root = ~
                 set vcs_root_abbr $vcs_root
@@ -63,7 +79,7 @@ function fish_prompt
             set paths[$vcs_basename_idx] $vcs_basename
         end
 
-        if test "$vcs_root" = /
+        if test "$real_vcs_root" = /
             set prompt "$color_path_highlight/$color_path"(string join / $paths[2..-1])
         else
             set prompt $color_path(string join / $paths)
